3729d88ae7209f72cf2f78eaf37a8fffaac9e496
Konrad Kulka <kk8346@student.pwsz-ns.edu.pl>
2022-11-14 02:47:07 +0100

    Generowanie klucza publicznego/prywatnego, szyfrowanie pliku Do naprawienia: błąd z odszyfrowaniem

--- Projekt1/Projekt1/Form1.Designer.cs
+++ Projekt1/Projekt1/Form1.Designer.cs
@@ -36,11 +36,21 @@ namespace Projekt1
             this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.saveFileDialog2 = new System.Windows.Forms.SaveFileDialog();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
-            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // openFileDialog1
@@ -51,7 +61,7 @@ namespace Projekt1
             // button1
             // 
             this.button1.AllowDrop = true;
-            this.button1.Location = new System.Drawing.Point(359, 62);
+            this.button1.Location = new System.Drawing.Point(580, 43);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(108, 23);
             this.button1.TabIndex = 0;
@@ -61,7 +71,7 @@ namespace Projekt1
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(220, 172);
+            this.button2.Location = new System.Drawing.Point(173, 202);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 23);
             this.button2.TabIndex = 1;
@@ -71,53 +81,135 @@ namespace Projekt1
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(492, 172);
+            this.button3.Location = new System.Drawing.Point(462, 202);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(75, 23);
             this.button3.TabIndex = 2;
             this.button3.Text = "Deszyfruj";
             this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(137, 43);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(437, 23);
+            this.textBox3.TabIndex = 7;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.SystemColors.Control;
+            this.label3.Location = new System.Drawing.Point(186, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(451, 15);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Wybierz plik do zaszyfrowania lub odszyfrowania z wykorzystaniem szyfrowania RSA:" +
+    "";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label4.Location = new System.Drawing.Point(12, 156);
+            this.label4.MaximumSize = new System.Drawing.Size(0, 2);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(0, 2);
+            this.label4.TabIndex = 9;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(173, 287);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(112, 47);
+            this.button4.TabIndex = 10;
+            this.button4.Text = "Wygeneruj klucz publiczny";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(425, 287);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(112, 47);
+            this.button5.TabIndex = 11;
+            this.button5.Text = "Wygeneruj klucz prywatny";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(331, 397);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 23);
+            this.button6.TabIndex = 12;
+            this.button6.Text = "button6";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(144, 224);
+            this.textBox1.Location = new System.Drawing.Point(137, 113);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(210, 23);
-            this.textBox1.TabIndex = 3;
-            this.textBox1.Text = "Klucz RSA public";
+            this.textBox1.Size = new System.Drawing.Size(204, 23);
+            this.textBox1.TabIndex = 13;
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(492, 224);
+            this.textBox2.Location = new System.Drawing.Point(372, 113);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(100, 23);
-            this.textBox2.TabIndex = 4;
-            this.textBox2.Text = "Klucz RSA private";
+            this.textBox2.Size = new System.Drawing.Size(202, 23);
+            this.textBox2.TabIndex = 14;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(220, 121);
+            this.label1.ForeColor = System.Drawing.SystemColors.Control;
+            this.label1.Location = new System.Drawing.Point(137, 85);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(70, 15);
-            this.label1.TabIndex = 5;
-            this.label1.Text = "Szyfrowanie";
+            this.label1.Size = new System.Drawing.Size(204, 15);
+            this.label1.TabIndex = 15;
+            this.label1.Text = "Wskaż lokalizację klucza publicznego:";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(492, 121);
+            this.label2.ForeColor = System.Drawing.SystemColors.Control;
+            this.label2.Location = new System.Drawing.Point(372, 85);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(83, 15);
-            this.label2.TabIndex = 6;
-            this.label2.Text = "Deszyfrowanie";
+            this.label2.Size = new System.Drawing.Size(202, 15);
+            this.label2.TabIndex = 16;
+            this.label2.Text = "Wskaż lokalizację klucza prywatnego:";
             // 
-            // textBox3
+            // button7
             // 
-            this.textBox3.Location = new System.Drawing.Point(203, 33);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(437, 23);
-            this.textBox3.TabIndex = 7;
+            this.button7.Location = new System.Drawing.Point(173, 142);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(136, 23);
+            this.button7.TabIndex = 17;
+            this.button7.Text = "Wybierz plik...";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(407, 142);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(130, 23);
+            this.button8.TabIndex = 18;
+            this.button8.Text = "Wybierz plik...";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // label5
+            // 
+            this.label5.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label5.Location = new System.Drawing.Point(12, 265);
+            this.label5.MaximumSize = new System.Drawing.Size(0, 2);
+            this.label5.MinimumSize = new System.Drawing.Size(0, 2);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(776, 2);
+            this.label5.TabIndex = 19;
             // 
             // Form1
             // 
@@ -125,16 +217,25 @@ namespace Projekt1
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.LightSeaGreen;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox3);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "deSZYfrator";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -147,11 +248,21 @@ namespace Projekt1
         protected System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog2;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Label label5;
     }
 }
 
--- Projekt1/Projekt1/Form1.cs
+++ Projekt1/Projekt1/Form1.cs
@@ -16,12 +16,7 @@ namespace Projekt1
         public Form1()
         {
             InitializeComponent();
-            var cryptoServiceProvider = new RSACryptoServiceProvider(2048); //2048 - Długość klucza
-            var privateKey = cryptoServiceProvider.ExportParameters(true); //Generowanie klucza prywatnego
-            var publicKey = cryptoServiceProvider.ExportParameters(false); //Generowanie klucza publiczny
 
-            //string publicKeyString = GetKeyString(publicKey);
-            //string privateKeyString = GetKeyString(privateKey);
         }
 
 
@@ -39,15 +34,183 @@ namespace Projekt1
             }
         }
 
-
-        private void button2_Click(object sender, EventArgs e)
+        private void button2_Click(object sender, EventArgs e) //szyfruj
         {
             string sciezka = textBox3.Text;
-            textBox1.Text = sciezka;
-            MessageBox.Show("Ścieżka to: " + sciezka);
+            string publicKey = textBox1.Text;
+            if (sciezka == "")
+            {
+                MessageBox.Show("Nie wybrano pliku, najpierw wskaż ścieżkę do pliku do zaszyfrowania");
+            }
+            else if (publicKey == "")
+            {
+                MessageBox.Show("Nie wybrano pliku klucza publicznego, spróbuj ponownie");
+            } else
+            {
+                string publicKeyText = System.IO.File.ReadAllText(publicKey);
+                string fileText = System.IO.File.ReadAllText(sciezka);
+                string encryptedText = Encrypt(fileText, publicKeyText);
+                MessageBox.Show(encryptedText);
+
+                SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+                saveFileDialog1.Filter = "Plik txt|*.txt";
+                saveFileDialog1.Title = "Wybierz lokalizację zapisu zaszyfrowanego pliku";
+                saveFileDialog1.ShowDialog();
+                if (saveFileDialog1.FileName != "")
+                {
+                    using (System.IO.FileStream fs = System.IO.File.Create(saveFileDialog1.FileName))
+                    {
+                        AddText(fs, encryptedText);
+                    }
+                }
+                MessageBox.Show("Zaszyfrowano plik \n Został zapisany w lokalizacji: \n " + saveFileDialog1.FileName);
+                System.Diagnostics.Process.Start("explorer.exe", saveFileDialog1.FileName);
+            }
+        }
+        
+
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
         }
 
+        private void button3_Click(object sender, EventArgs e) //deszyfruj
+        {
+            string sciezka = textBox3.Text;
+            string privateKey = textBox2.Text;
+            if (sciezka == "")
+            {
+                MessageBox.Show("Nie wybrano pliku, najpierw wskaż ścieżkę do pliku do zaszyfrowania");
+            }
+            else if (privateKey == "")
+            {
+                MessageBox.Show("Nie wybrano pliku klucza prywatnego, spróbuj ponownie");
+            }
+            else
+            {
+                string privateKeyText = System.IO.File.ReadAllText(privateKey);
+                string fileText = System.IO.File.ReadAllText(sciezka);
+                string encryptedText = Decrypt(fileText, privateKeyText);
+                MessageBox.Show(privateKeyText);
+                MessageBox.Show(fileText);
+                MessageBox.Show(encryptedText);
+                /* SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+                 saveFileDialog1.Filter = "Plik txt|*.txt";
+                 saveFileDialog1.Title = "Wybierz lokalizację zapisu klucza prywatnego";
+                 saveFileDialog1.ShowDialog();
+                 if (saveFileDialog1.FileName != "")
+                 {
+                     using (System.IO.FileStream fs = System.IO.File.Create(saveFileDialog1.FileName))
+                     {
+                         AddText(fs, encryptedText);
+                     }
+                 }
+                 MessageBox.Show("Odszyfrowano plik \n Został zapisany w lokalizacji: \n " + saveFileDialog1.FileName);
+                 System.Diagnostics.Process.Start("explorer.exe", saveFileDialog1.FileName);*/
+            }
+        }
 
+        private void splitContainer1_Panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e) //generuj klucz publiczny
+        {
+            var cryptoServiceProvider = new RSACryptoServiceProvider(2048); //2048 - Długość klucza
+            var publicKey = cryptoServiceProvider.ExportParameters(false); //Generowanie klucza publiczny
+            string publicKeyString = GetKeyString(publicKey);
+            SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+            saveFileDialog1.Filter = "Plik XML|*.xml";
+            saveFileDialog1.Title = "Wybierz lokalizację zapisu klucza publicznego";
+            saveFileDialog1.FileName = "publickey";
+            saveFileDialog1.ShowDialog();
+            if (saveFileDialog1.FileName != "")
+            {
+            using (System.IO.FileStream fs = System.IO.File.Create(saveFileDialog1.FileName))
+                {
+                    AddText(fs, publicKeyString);
+                }
+            }
+         
+
+
+            MessageBox.Show("Wygenerowano klucz publiczny \n Został zapisany w pliku tekstowym w lokalizacji: \n " + saveFileDialog1.FileName);
+            System.Diagnostics.Process.Start("explorer.exe", saveFileDialog1.FileName);
+        }
+
+        public static void AddText(System.IO.FileStream fs, string value)
+        {
+            byte[] info = new UTF8Encoding(true).GetBytes(value);
+            fs.Write(info, 0, info.Length);
+        }
+
+        private void button5_Click(object sender, EventArgs e) //generuj klucz prywatny
+        {
+            var cryptoServiceProvider = new RSACryptoServiceProvider(2048); //2048 - Długość klucza
+            var privateKey = cryptoServiceProvider.ExportParameters(true); //Generowanie klucza prywatnego
+            string privateKeyString = GetKeyString(privateKey);
+            SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+            saveFileDialog1.Filter = "Plik XML|*.xml";
+            saveFileDialog1.Title = "Wybierz lokalizację zapisu klucza publicznego";
+            saveFileDialog1.FileName = "privatekey";
+            saveFileDialog1.ShowDialog();
+            if (saveFileDialog1.FileName != "")
+            {
+                //System.IO.FileStream saveFile = (System.IO.FileStream)saveFileDialog1.OpenFile();
+                using (System.IO.FileStream fs = System.IO.File.Create(saveFileDialog1.FileName))
+                {
+                    AddText(fs, privateKeyString);
+                }
+
+
+
+            }
+
+            MessageBox.Show("Wygenerowano klucz prywatny \n Został zapisany w pliku XML w lokalizacji: \n " + saveFileDialog1.FileName);
+            System.Diagnostics.Process.Start("explorer.exe", saveFileDialog1.FileName);
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            string sciezka = textBox3.Text;
+            string text = System.IO.File.ReadAllText(sciezka);
+            MessageBox.Show(text);
+        }
+
+        private void button7_Click(object sender, EventArgs e) //szyfruj wybierz plik
+        {
+            using (OpenFileDialog pubkey = new OpenFileDialog())
+            {
+                pubkey.Filter = "Wszystkie pliki|*";
+                pubkey.Title = "Wybierz plik";
+                if (pubkey.ShowDialog() != DialogResult.OK) return;
+                string filePath = pubkey.FileName;
+                string fileName = pubkey.SafeFileName;
+                string filePathName = pubkey.FileName.Replace(pubkey.SafeFileName, "");
+                textBox1.Text = filePath;
+            }
+        }
+
+        private void button8_Click(object sender, EventArgs e) //deszyfruj wybierz plik
+        {
+            using (OpenFileDialog privkey = new OpenFileDialog())
+            {
+                privkey.Filter = "Wszystkie pliki|*";
+                privkey.Title = "Wybierz plik";
+                if (privkey.ShowDialog() != DialogResult.OK) return;
+                string filePath = privkey.FileName;
+                string fileName = privkey.SafeFileName;
+                string filePathName = privkey.FileName.Replace(privkey.SafeFileName, "");
+                textBox2.Text = filePath;
+            }
+        }
 
         //początek skryptu na szyfrowanie/deszyfrowanie
 
@@ -104,18 +267,6 @@ namespace Projekt1
             }
         }
 
-        private static string GenerateTestString()
-        {
-            Guid opportinityId = Guid.NewGuid();
-            Guid systemUserId = Guid.NewGuid();
-            string currentTime = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
 
-            StringBuilder sb = new StringBuilder();
-            sb.AppendFormat("opportunityid={0}", opportinityId.ToString());
-            sb.AppendFormat("&systemuserid={0}", systemUserId.ToString());
-            sb.AppendFormat("&currenttime={0}", currentTime);
-
-            return sb.ToString();
-        }
     }
 }
