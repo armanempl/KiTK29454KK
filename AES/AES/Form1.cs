ffddcd95756826b4a66d1f9809b65b0fab71374b
Konrad Kulka <kk8346@student.pwsz-ns.edu.pl>
2022-12-13 23:58:39 +0100

    AES - szyfrowanie i odszyfrowanie tekstu z generacją Key i IV

---
+++ AES/AES/Form1.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AES
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        byte[] AESKey;
+        byte[] AESIV;
+        byte[] encryptedText;
+        string decryptedText;
+        private void GenerateKeyIV_Click(object sender, EventArgs e)
+        {
+            using (RijndaelManaged rijndael = new RijndaelManaged())
+            {
+                rijndael.GenerateKey();
+                rijndael.GenerateIV();
+                AESKey = rijndael.Key;
+                AESIV = rijndael.IV;
+                KeyText.Text = Encoding.UTF8.GetString(AESKey);
+                IVText.Text = Encoding.UTF8.GetString(AESIV);
+            }
+
+        }
+
+        public static byte[] AESEncrypt(string text, byte[] Key, byte[] IV)
+        {
+            byte[] tempEnc;
+            using (RijndaelManaged rij = new RijndaelManaged())
+            {
+                rij.Key = Key;
+                rij.IV = IV;
+                ICryptoTransform encrypt = rij.CreateEncryptor(rij.Key, rij.IV);
+                using (MemoryStream msEnc = new MemoryStream())
+                {
+                    using (CryptoStream csEnc = new CryptoStream(msEnc, encrypt, CryptoStreamMode.Write))
+                    {
+                        using (StreamWriter swEnc = new StreamWriter(csEnc))
+                        {
+                            swEnc.Write(text);
+                        }
+                    tempEnc = msEnc.ToArray();
+                    }
+                }
+            }
+            return tempEnc;
+        }
+
+        public static string AESDecrypt(byte[] encryptedtext, byte[] Key, byte[] IV)
+        {
+            string decryptedText = null;
+
+            using (RijndaelManaged rijd = new RijndaelManaged())
+            {
+                rijd.Key = Key;
+                rijd.IV = IV;
+
+                ICryptoTransform decrypt = rijd.CreateDecryptor(rijd.Key, rijd.IV);
+                using (MemoryStream msDc = new MemoryStream(encryptedtext))
+                {
+                    using (CryptoStream csDc = new CryptoStream(msDc, decrypt, CryptoStreamMode.Read))
+                    {
+                        using (StreamReader srDc = new StreamReader(csDc))
+                        {
+                            decryptedText = srDc.ReadToEnd();
+                        }
+                    }
+                }
+            }
+            return decryptedText;
+        
+        }
+
+        private void EncryptButton_Click(object sender, EventArgs e)
+        {
+            byte[] enc = AESEncrypt(UserInput.Text, AESKey, AESIV);
+            encryptedText = enc;
+            AESOutput.Text = Encoding.UTF8.GetString(enc);
+        }
+
+        private void DecryptButton_Click(object sender, EventArgs e)
+        {
+            string dec = AESDecrypt(encryptedText, AESKey, AESIV);
+            decryptedText = dec;
+            UserInput.Text = dec;
+        }
+    }
+}
